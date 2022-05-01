b4ebf1f3c034fd5ca72e152ce0114e6a12679356
Cayden S <cayden.schmandt@ndsu.edu>
2022-05-01 16:02:37 -0500

    Branch merges

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import {User} from '../objects/User';
+import {USERS} from '../objects/Userdb'
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,23 @@ import {User} from '../objects/User';
 export class UsersService {
 
   constructor() { }
+
+  getAccountData(): Observable<User[]>{
+    return of(USERS);
+  }
+
+  getUserbyID(id: number):Observable<User>{
+    let user: User = new User();
+    USERS.forEach((value)=>{
+      let done: boolean=false;
+      if(!done){
+        if(user.id == id){
+          user = value;
+          done = true;
+        }
+      }
+      return;
+    });
+    return of(user);
+  }
 }
