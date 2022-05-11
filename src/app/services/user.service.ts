24e055c6ba18fb27824dd41a327fec6fed13f850
Cole Gartner <cgartner37@gmail.com>
2022-05-10 21:04:08 -0500

    Changed up a bit of stuff

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import { User } from '../objects/user';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
